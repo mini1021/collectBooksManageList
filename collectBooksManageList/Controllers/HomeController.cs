bea6815b9b2fd9a310c7dfeae2c73ad2252518ba
LBJ <LBJ>
2017-06-29 14:33:16 +0800

    View

---
+++ collectBooksManageList/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace collectBooksManageList.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult BookList()
+        {
+            return View();
+        }
+        public ActionResult Registered()
+        {
+            return View();
+        }
+        public ActionResult EditBook()
+        {
+            return View();
+        }
+    }
+}
